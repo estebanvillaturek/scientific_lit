a33f91f70c4d3aea88287bdac9bcf8c9319d57ea
Esteban Villa-Turek <42799967+estebanvillaturek@users.noreply.github.com>
2020-09-16 16:08:09 -0500

    query with logical terms

--- full_text.py
+++ full_text.py
@@ -8,17 +8,17 @@ con_file = open("config.json")
 client = json.load(con_file)
 con_file.close()
 
-GET_ALL = False # False gets one chunk (25) True gets all or max (5000)
+GET_ALL = True # False gets one chunk (25) True gets all or max (5000)
 FULLTEXT = False # Save fulltext
 OPENACCESS = True # Search only openaccess documents (so we can get the full text)
 
-query = "public policy"
+query = "(public policy OR impact OR result OR evaluation OR evidence AND climate OR environment)"
 
 if OPENACCESS:
   query = "openaccess(1) AND " + query
 
 doc_srch = ElsSearch(query,'sciencedirect')
-doc_srch.execute(client, get_all = GET_ALL)
+doc_srch.execute(ElsClient(client['apikey']), get_all = GET_ALL)
 
 print ("# Found", len(doc_srch.results), "results.")
 
@@ -28,7 +28,7 @@ for doc in doc_srch.results:
   if FULLTEXT:
     ## ScienceDirect (full-text) document example using DOI
     doi_doc = FullDoc(doi = doi)
-    if doi_doc.read(client):
+    if doi_doc.read(ElsClient(client['apikey'])):
         doi_doc.write() 
     else:
         print ("Read full-text failed for DOI" ,doi)
